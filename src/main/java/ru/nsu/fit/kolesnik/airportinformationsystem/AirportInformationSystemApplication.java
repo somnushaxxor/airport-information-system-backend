a7a09c9ba6d813b5aed51678bf03108ff34818e5
Artem Kolesnik <steeltouch27@gmail.com>
2023-05-02 13:59:42 +0700

    Init project

---
+++ src/main/java/ru/nsu/fit/kolesnik/airportinformationsystem/AirportInformationSystemApplication.java
@@ -0,0 +1,13 @@
+package ru.nsu.fit.kolesnik.airportinformationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirportInformationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirportInformationSystemApplication.class, args);
+    }
+
+}
