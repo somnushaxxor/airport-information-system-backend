9e5ba6a6684c1af2028930a1c332470d5f6d5f8a
Artem Kolesnik <steeltouch27@gmail.com>
2023-05-15 15:02:02 +0700

    Add Deleting dep chief exception

---
+++ src/main/java/ru/nsu/fit/kolesnik/airportinformationsystem/DeletingDepartmentChiefException.java
@@ -0,0 +1,13 @@
+package ru.nsu.fit.kolesnik.airportinformationsystem;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class DeletingDepartmentChiefException extends RuntimeException {
+
+    public DeletingDepartmentChiefException() {
+        super("Unable to delete the chief of the department");
+    }
+
+}
